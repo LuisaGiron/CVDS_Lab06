50a7fe129e74c56cd6af960575f99e60f83b75ab
LuisaGiron <luisa.bermudez@mail.escuelaing.edu.co>
2022-09-21 17:38:12 -0500

    avance parte 2 hasta el punto 9

---
+++ web/src/main/java/edu/eci/cvds/servlet/model/Todo.java
@@ -0,0 +1,49 @@
+package edu.eci.cvds.servlet.model;
+
+/*
+ * @author: Luisa Bermudez - Daniela Ladino
+ * @version: 1 (21/09/2022)
+ */
+public class Todo {
+
+	private int userId;
+	private int id;
+	private String title;
+	private boolean completed;
+	
+	public Todo() {
+		
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public boolean getCompleted() {
+		return completed;
+	}
+	
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+}
